c3ea6953dcf583f42848c63fa2b9ef68f67998ed
Maxim Gavrilov <Maxim.Gavrilov@online-express.ru>
2024-02-19 16:33:31 +0300

    Warning:(7, 7) Class name doesn't match the containing file name

--- database/factories/UsersSessionFactory.php
+++ database/factories/UsersSessionFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class usersSessionFactory extends Factory
+class UsersSessionFactory extends Factory
 {
     /**
      * Define the model's default state.
